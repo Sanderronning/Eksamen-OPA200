43647b30403159b3fdd59476ffd6e778dedd6d17
Sanderronning <ronning.sander@gmail.com>
2025-11-05 12:10:04 +0100

    tar en sjanse

--- src/main/java/controller/reportController.java
+++ src/main/java/controller/reportController.java
@@ -19,7 +19,11 @@ public class reportController {
 		this.reportView = reportView;
 		this.reportFileWriter = reportFileWriter;
 	}
+<<<<<<< Updated upstream
 
 
 	
+=======
+	//123r4
+>>>>>>> Stashed changes
 }
