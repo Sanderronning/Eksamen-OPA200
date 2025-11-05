463da1791ee8a19fdb1de0ab6bc7dc0216e0b2f7
Raheleh <tajiknerges7@gmail.com>
2025-11-05 11:34:19 +0100

    Update teacherController.java

--- src/main/java/controller/teacherController.java
+++ src/main/java/controller/teacherController.java
@@ -1,4 +1,44 @@
 packeg controller;
 
 import javafx.fxml.FXML;
-imprt javafx.
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import dao.TeacherDAO;
+import model.Teacher;
+
+public class teacherController{
+	@FXML private TextField nameField;
+    @FXML private TextField emailField;
+    @FXML private TextField courseField;
+    @FXML private TextField tlfNrField;
+    @FXML private Label statusLabel;
+    
+    private teacherDao teacherDao = new teacherDao();
+    
+    @FXML
+    private void registerTeacher() {
+    	String name = nameField.getText();
+    	String email = emailField.getText();
+        String course = courseField.getText();
+        String tlfNr = tlfNrField.getText();
+        
+        if (name == null || name.trim().isEmpty() || email == null || email.trim().isEmpty()){
+            statusLabel.setText("Name and Email are required!");
+            return;
+        }
+        
+        teacher teacher = new teacher(name, email, course, tlfNr);
+        teacherDao.registerTeacher(teacher);
+
+        statusLabel.setText("Teacher registered successfully!");
+        clearFields();
+    }
+
+    private void clearFields() {
+        nameField.clear();
+        emailField.clear();
+        courseField.clear();
+        tlfNrField.clear();
+    }
+}
+
